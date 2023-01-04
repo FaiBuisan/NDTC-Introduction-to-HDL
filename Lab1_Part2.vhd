16ce0a1afa5d96f14196398d86244574f9043955
Fai <119716357+FaiBuisan@users.noreply.github.com>
2023-01-04 20:31:50 +0800

    Update Lab1_Part2.vhd

--- Lab1_Part2.vhd
+++ Lab1_Part2.vhd
@@ -1,32 +1,28 @@
-LIBRARY ieee;
+lIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
-ENTITY Part2 IS 
-	PORT ( 	
-			s 				: IN STD_LOGIC;
-			x0,x1,x2,x3 : IN STD_LOGIC;
-			y0,y1,y2,y3 : IN STD_LOGIC;
-			m0,m1,m2,m3	: OUT STD_LOGIC); -- red LEDs
+ENTITY Lab1_Part2 IS
+port(
 
-		END Part2;
+S : IN STD_LOGIC;
+Y : IN STD_LOGIC;
+X : IN STD_LOGIC;
+M : OUT STD_LOGIC
 
-		ARCHITECTURE Behavior OF Part2 IS
+);
+		
+END Lab1_Part2;
 
-	component Part3 IS
+ARCHITECTURE Behavior OF Lab1_Part2 IS
+SIGNAL A: STD_LOGIC;
+SIGNAL B: STD_LOGIC;
 
+BEGIN
 
-			PORT ( s,x,y : IN STD_LOGIC;
-			
-		 m		 : OUT STD_LOGIC); -- red LEDs
+A <= X AND(NOT(S));
+B <= Y AND S;
 
-			END component;
-
-	BEGIN
-
-		com1 : Part3 port map (s,x0,y0,m0);
-		com2 : Part2 port map (s,x1,y1,m1);
-		com3 : Part2 port map (s,x2,y2,m2);
-		com4 : Part2 port map (s,x3,y3,m3);
+M<= A OR B;
 
 
-		END Behavior;
+END Behavior;
